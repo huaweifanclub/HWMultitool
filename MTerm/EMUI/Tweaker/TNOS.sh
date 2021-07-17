9f8d816fe5195ec5e315c6a6b326451bb7dc9465
alpinealps <60939957+fsvk74@users.noreply.github.com>
2021-07-17 15:39:12 +0200

    Hot fix

--- MTerm/EMUI/Tweaker/TNOS.sh
+++ MTerm/EMUI/Tweaker/TNOS.sh
@@ -1,6 +1,6 @@
 select yn in "Yes" "No"; do
 	case $yn in
-		Yes ) adb shell settings system put theme_no_online 1 && exit; break;;
-		No ) adb shell settings system put theme_no_online 0 && exit;;
+		Yes ) adb shell settings put system theme_no_online 1 && exit; break;;
+		No ) adb shell settings put system theme_no_online 0 && exit;;
 	esac
 done
