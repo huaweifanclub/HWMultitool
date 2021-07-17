3882ba65322c5fc2b3eb2cf3213a602c7dd72897
alpinealps <60939957+fsvk74@users.noreply.github.com>
2021-07-17 15:34:24 +0200

    Hot fix

--- MTerm/EMUI/Tweaker/ANTLS.sh
+++ MTerm/EMUI/Tweaker/ANTLS.sh
@@ -1,6 +1,6 @@
 select yn in "Yes" "No"; do
 	case $yn in
-		Yes ) adb shell settings system put app_name_two_lines 1 && exit; break;;
-		No ) adb shell settings system put app_name_two_lines 0 && exit;;
+		Yes ) adb shell settings put system app_name_two_lines 1 && exit; break;;
+		No ) adb shell settings put system app_name_two_lines 0 && exit;;
 	esac
 done
