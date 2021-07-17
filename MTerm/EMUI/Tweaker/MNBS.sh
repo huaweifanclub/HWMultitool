ac0d42c2c01852b3dc679c2f3dfc4f23472e3d3d
alpinealps <60939957+fsvk74@users.noreply.github.com>
2021-07-17 15:37:34 +0200

    Hot fix

--- MTerm/EMUI/Tweaker/MNBS.sh
+++ MTerm/EMUI/Tweaker/MNBS.sh
@@ -1,6 +1,6 @@
 select yn in "Yes" "No"; do
 	case $yn in
-		Yes ) adb shell settings system put navigationbar_is_min 1 && exit; break;;
-		No ) adb shell settings system put navigationbar_is_min 0 && exit;;
+		Yes ) adb shell settings put system navigationbar_is_min 1 && exit; break;;
+		No ) adb shell settings put system navigationbar_is_min 0 && exit;;
 	esac
 done
