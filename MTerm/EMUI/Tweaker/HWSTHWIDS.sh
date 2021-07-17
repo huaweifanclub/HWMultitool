16f3a9224770d5485c88948d47ca2dff27d0e314
alpinealps <60939957+fsvk74@users.noreply.github.com>
2021-07-17 15:37:15 +0200

    Hot fix

--- MTerm/EMUI/Tweaker/HWSTHWIDS.sh
+++ MTerm/EMUI/Tweaker/HWSTHWIDS.sh
@@ -1,6 +1,6 @@
 select yn in "Yes" "No"; do
 	case $yn in
-		Yes ) adb shell settings system put hw_theme_support_hwid 1 && exit; break;;
-		No ) adb shell settings system put hw_theme_support_hwid 0 && exit;;
+		Yes ) adb shell settings put system hw_theme_support_hwid 1 && exit; break;;
+		No ) adb shell settings put system hw_theme_support_hwid 0 && exit;;
 	esac
 done
