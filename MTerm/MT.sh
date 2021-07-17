16b498c5aaa64f1b4e8fb85f388a01b2b8634f5c
alpinealps <60939957+fsvk74@users.noreply.github.com>
2021-07-17 18:57:01 +0200

    Updated!

--- MTerm/MT.sh
+++ MTerm/MT.sh
@@ -2,12 +2,12 @@
 
 clear
 echo "Welcome to HUAWEI/Android Console MultiTool!"
-echo "Version: 0.1.2 Stable, developer: TeamMT (alpinealps, old nickname: fsvk74)"
+echo "Version: 0.1.3 Stable, developer: TeamMT (alpinealps, old nickname: fsvk74)"
 echo "LICENSE - MIT (For ALL Scripts in THIS MultiTool)"
 echo "If yo want to donate me (RUS) please, contact me on 4PDA ^^"
 echo "Last IMEI, Last full information from build.prop and Last Unlock Code will be saved in EMUI/Backups"
 PS3='Please enter your choice (Press Enter, if you don`t see menu): '
-options=("Get info about phone" "FI Flash Program" "FI Unlock Program" "Deleter" "PowerGenue" "EMUI Tweaker" "Samsung Flash Program" "Setup Program" "Exit")
+options=("Get info about phone" "FI Flash Program" "FI Unlock Program" "Deleter" "Deleter_Groups" "PowerGenue" "EMUI Tweaker" "Samsung Flash Program" "Setup Program" "Get default install location" "Set default install location" "Exit")
 select opt in "${options[@]}"
 do
     case $opt in
@@ -30,8 +30,10 @@ do
 	"FI Unlock Program")
 	    bash EMUI/Unlocker.sh
 	    ;;
-	"Deleter")
-		echo "Now working: Xiaomi, AOSP Programs, Launcher and.., SwiftKey, Google and HUAWEI."
+	"Delete apps manually")
+		bash EMUI/Deleter.sh
+	;;
+	"Deleter_Groups")
 		bash EMUI/Deleter.sh
 	    ;;
 	"EMUI Tweaker")
@@ -49,6 +51,13 @@ do
 		clear
 		bash EMUI/Setuper.sh
 	    ;;
+	"Get default install location")
+		echo "0 - Auto (default), 1 - internal, 2 - external)"
+		adb shell pm get-install-location
+	;;
+	"Set default install location")
+		bash EMUI/SDIL.sh
+	;;
         "Exit")
             break
             ;;
