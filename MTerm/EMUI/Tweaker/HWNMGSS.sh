969403ff9b79cf27992510abfe55aa2f4df793e2
alpinealps <60939957+fsvk74@users.noreply.github.com>
2021-07-17 15:36:30 +0200

    Hot fix

--- MTerm/EMUI/Tweaker/HWNMGSS.sh
+++ MTerm/EMUI/Tweaker/HWNMGSS.sh
@@ -1,6 +1,6 @@
 select yn in "Yes" "No"; do
 	case $yn in
-		Yes ) adb shell settings system put hw_networkmode_gstyle 1 && exit; break;;
-		No ) adb shell settings system put hw_networkmode_gstyle 0 && exit;;
+		Yes ) adb shell settings put system hw_networkmode_gstyle 1 && exit; break;;
+		No ) adb shell settings put system hw_networkmode_gstyle 0 && exit;;
 	esac
 done
