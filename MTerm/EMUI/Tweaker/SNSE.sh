42836a8285c9121b9af1a991f0e0e458eef6a72f
alpinealps <60939957+fsvk74@users.noreply.github.com>
2021-07-17 15:38:29 +0200

    Hot fix

--- MTerm/EMUI/Tweaker/SNSE.sh
+++ MTerm/EMUI/Tweaker/SNSE.sh
@@ -1,6 +1,6 @@
 select yn in "Yes" "No"; do
 	case $yn in
-		Yes ) adb shell settings system put show_network_speed_enabled 1 && exit; break;;
-		No ) adb shell settings system put show_network_speed_enabled 0 && exit;;
+		Yes ) adb shell settings put system show_network_speed_enabled 1 && exit; break;;
+		No ) adb shell settings put system show_network_speed_enabled 0 && exit;;
 	esac
 done
