26b723b782003c74e1114a028ebe9595680a2590
alpinealps <60939957+fsvk74@users.noreply.github.com>
2021-07-17 15:36:50 +0200

    Hot fix

--- MTerm/EMUI/Tweaker/HWNMPS.sh
+++ MTerm/EMUI/Tweaker/HWNMPS.sh
@@ -1,6 +1,6 @@
 select yn in "Yes" "No"; do
 	case $yn in
-		Yes ) adb shell settings system put hw_networkmode_preference 1 && exit; break;;
-		No ) adb shell settings system put hw_networkmode_preference 0 && exit;;
+		Yes ) adb shell settings put system hw_networkmode_preference 1 && exit; break;;
+		No ) adb shell settings put system hw_networkmode_preference 0 && exit;;
 	esac
 done
