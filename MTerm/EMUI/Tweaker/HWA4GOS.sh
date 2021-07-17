df329518d827c086c53f34e76b2ed0d05e5d9245
alpinealps <60939957+fsvk74@users.noreply.github.com>
2021-07-17 15:35:05 +0200

    Hot fix

--- MTerm/EMUI/Tweaker/HWA4GOS.sh
+++ MTerm/EMUI/Tweaker/HWA4GOS.sh
@@ -1,6 +1,6 @@
 select yn in "Yes" "No"; do
 	case $yn in
-		Yes ) adb shell settings system put hw_add_4g_only 1 && exit; break;;
-		No ) adb shell settings system put hw_add_4g_only 0 && exit;;
+		Yes ) adb shell settings put system hw_add_4g_only 1 && exit; break;;
+		No ) adb shell settings put system hw_add_4g_only 0 && exit;;
 	esac
 done
